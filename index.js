5761e8d320369719c8e1de6019e8efe6d28bf725
Ussen_Kimanuka <ukimanuka@gmail.com>
2023-10-02 08:24:57 +0300

    working with arthmetics

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31 ;
+let num2 =2 ;
+let multiply = num1*num2;
+
+let myNumber = Math.random();
+
+let random = Math.ceil(myNumber);
+
+
+let num3 =24 ;
+let num4 =5 ;
+
+let mod = num3%num4;
+
+
+let max =Math.max(15, 2, 19, 20);
